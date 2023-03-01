8c7ce668711e542d12872d44db74e7805178bf56
rennavittorio <renna.vittorio@gmail.com>
2023-03-01 15:57:08 +0100

    WIP header comp wip

--- vite-comics-project/src/components/AppFooter.vue
+++ vite-comics-project/src/components/AppFooter.vue
@@ -12,6 +12,6 @@ export default {
 
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
--- vite-comics-project/src/components/AppHeader.vue
+++ vite-comics-project/src/components/AppHeader.vue
@@ -10,8 +10,77 @@ export default {
 
 <template>
 
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="logo-wrapper">
+                        <img src="/images/dc-logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <nav class="nav-bar">
+                        <ul class="nav__list">
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="#">link</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+.row {
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nav__list {
+    display: flex;
+
+    .nav__item a {
+        padding: 0 10px;
+    }
+
+    &::after {
+        content: '';
+        display: block;
+        width: 10px;
+        padding: 10px;
+        background-color: $main-blue;
+    }
+}
 
 </style>
